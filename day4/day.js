5dcd2723b5f2f0ba418afb1233d834125a122c75
jaskaranjs <147585489+jaskaranjs@users.noreply.github.com>
2023-10-19 05:34:11 +0000

    done

---
+++ day4/day.js
@@ -0,0 +1,40 @@
+// let information = {
+//     name:"jaskaran",
+//     age:17,
+// }
+
+
+// let information2 = information
+//  information2.age="45" 
+
+
+
+    // console.log(information2)
+
+
+// let name1 = ["jaskaran","singh","jashan"]
+// let name2 = name1
+
+// console.log(name2)
+
+
+// let name1 = "jaskaran"
+// let = name2 = name1
+// name2 = "jassi"
+// console.log (name2)
+// // console.log(name1)
+
+
+
+// let person = {
+
+// name :"jaskaran" ,
+// age :17  ,
+
+// }
+//  let person1 = person
+//  person1.name = "jassi"
+//  console.log (person) 
+//  console.log (person1) 
+
+
--- text.js
+++ text.js
@@ -1,1 +1,54 @@
-console.log("jaskaran singh")
+
+ let age = 17;
+ let valueonstring = String(age); 
+ console.log (typeof age);
+ console.log (valueonstring);
+ console.log (typeof valueonstring);
+
+
+
+
+let statement = true
+let valueofstring = String(statement)
+console.log (typeof statement)
+console.log (valueofstring)
+console.log (typeof valueofstring)
+
+
+
+ let length = 1.7;
+ let valueinstring = String(length);
+ console.log (typeof length)
+ console.log (valueinstring)
+ console.log (typeof valueinstring)
+
+
+let a = "abc"
+
+let b = 123
+
+let c = true
+
+let d = null
+
+let e = undefined
+
+let f = BigInt
+
+let g = Symbol
+
+let h = Array
+
+let i = Object
+
+
+console.log(a,b,c,d,e,f,g,h,i)
+
+
+
+
+let as = +45
+let posvalue = -as
+console.log(posvalue)
+f
+console.log(1+"45"+45)
